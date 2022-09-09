1224798590674642a6bb49526d8a861cc890d97e
JacquesNel12 <jacquesnel85@gmail.com>
2022-09-09 14:15:52 +0200

    Update Insert_Statements.sql

--- Insert_Statements.sql
+++ Insert_Statements.sql
@@ -7,3 +7,6 @@ VALUES ('BK1','M01', 'FT322', 22-09-15)
 INSERT INTO DESTINATION (Destination_ID, Airport_Name, City_Name, Province)
 VALUES ('CPT', 'Cape Airlines', 'CapeTown', 'Western Cape')
 
+INSERT INTO PLANE (Plane_Number, Capacity, Airplane_Name)
+VALUES ('P1', 11, 'Fasty')
+
