c73296c7f0eeb6bf5ea88519131e8ebba6a584d5
JacquesNel12 <jacquesnel85@gmail.com>
2022-09-09 12:11:16 +0200

    Update Create_Tables.sql
    Finished Creating Tables For The Database :).

--- Create_Tables.sql
+++ Create_Tables.sql
@@ -6,3 +6,48 @@ CREATE TABLE PLANE (
     Capacity INT,
     Airplane_Name VARCHAR(64)
 );
+
+CREATE TABLE MANAGEMENT (
+    Management_ID VARCHAR(36) PRIMARY KEY,
+    First_Name VARCHAR(64),
+    Last_Name VARCHAR(64),
+    Passwords VARCHAR(64),
+    Email VARCHAR(64),
+    ID_Number INT,
+    Role_ID VARCHAR(36)
+);
+
+CREATE TABLE MEMBER (
+    Member_ID VARCHAR(36) PRIMARY KEY,
+    First_Name VARCHAR(64),
+    Last_Name VARCHAR (64),
+    Passwords VARCHAR(64),
+    Email VARCHAR(64),
+    ID_Number INT
+);
+
+CREATE TABLE DESTINATION (
+    Destination_ID VARCHAR(3) PRIMARY KEY,
+    Airport_Name VARCHAR(64),
+    City_Name VARCHAR(64),
+    Province VARCHAR(64)
+);
+
+CREATE TABLE FLIGHTS (
+    Flight_Number VARCHAR(36) PRIMARY KEY,
+    Destination_ID VARCHAR(3) FOREIGN KEY REFERENCES DESTINATION(Destination_ID),
+    Departure_Date DATETIME,
+    Departure_Time DATETIME,
+    Plane_Number VARCHAR(36) FOREIGN KEY REFERENCES PLANE(Plane_Number),
+    Gate_Number VARCHAR(36),
+    Status VARCHAR(64),
+    Price SMALLMONEY
+);
+
+CREATE TABLE BOOKING (
+    Booking_Number VARCHAR(36) PRIMARY KEY,
+    Member_ID VARCHAR(36) FOREIGN KEY REFERENCES MEMBER(Member_ID),
+    Flight_Number VARCHAR(36) FOREIGN KEY REFERENCES FLIGHTS(Flight_Number),
+    Booking_Date DATETIME
+
+);
